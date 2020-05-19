fc9408b37b36f1b9a8b749310eb867f4e3506b79
VINAYAKVISHWANATH, GOSAIN <vinayakvishwanath.gosain@nissanmotor.com>
2020-05-19 17:50:13 +0530

    3-reveal-module starter_code

---
+++ 3-reveal-module/starter_code.js
@@ -0,0 +1,15 @@
+//revealing module pattern
+//Turn this object literal into a module that only exposes the render method
+
+let myModule = {
+  data: [],
+  render: () => {
+
+  },
+  add: () => {
+
+  },
+  remove: () => {
+
+  }
+};
